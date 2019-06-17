be684afc9d87317aa95a6e589fb5f0c98bcf6cbd
Randy Coburn <morfien101@gmail.com>
2019-06-17 15:17:18 +0100

    Added in support for json output. Added the ability to base64 encode the output. USeful for camparing output from multiple keys. Added pretty printing.

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -14,14 +16,17 @@ var (
 	VERSION = "0.0.1"
 
 	// Flags
-	flagPath      = flag.String("path", "", "Parameter Store path")
-	flagRecursive = flag.Bool("recursive", false, "Look up all keys in branch")
-	flagDecrypt   = flag.Bool("decrypt", false, "Request decrypted keys")
-	flagAccessKey = flag.String("access-key", "", "Access key for AWS API")
-	flagSecretKey = flag.String("secret-key", "", "Secret key for AWS API")
-	flagRegion    = flag.String("region", "", "Region for AWS API")
-	flagHelp      = flag.Bool("h", false, "Help menu")
-	flagVersion   = flag.Bool("v", false, "Show Version")
+	flagPath       = flag.String("path", "", "Parameter Store path")
+	flagBase64     = flag.Bool("base64", false, "Base64 encode collected values.")
+	flagJSONMulti  = flag.Bool("json-output", false, "Output multiple key, values as JSON")
+	flagJSONPretty = flag.Bool("json-pretty", false, "JSON output is displayed in pretty print")
+	flagRecursive  = flag.Bool("recursive", false, "Look up all keys in branch")
+	flagDecrypt    = flag.Bool("decrypt", false, "Request decrypted keys")
+	flagAccessKey  = flag.String("access-key", "", "Access key for AWS API")
+	flagSecretKey  = flag.String("secret-key", "", "Secret key for AWS API")
+	flagRegion     = flag.String("region", "", "Region for AWS API")
+	flagHelp       = flag.Bool("h", false, "Help menu")
+	flagVersion    = flag.Bool("v", false, "Show Version")
 )
 
 func main() {
@@ -62,14 +67,45 @@ func main() {
 		if err != nil {
 			log.Fatalf("Failed to read from parameter store. Error: %s", err)
 		}
-		for key, value := range values {
-			fmt.Printf("%s:%s\n", key, value)
+
+		// If base64 is requested then convert the values and store them in place of the current values.
+		if *flagBase64 {
+			for key, value := range values {
+				values[key] = b64(value)
+			}
 		}
+		switch {
+		case *flagJSONMulti:
+			var out []byte
+			var err error
+			if *flagJSONPretty {
+				out, err = json.MarshalIndent(values, "", "  ")
+			} else {
+				out, err = json.Marshal(values)
+			}
+
+			if err != nil {
+				log.Fatalf("Failed to convert to JSON. Error: %s", err)
+			}
+			fmt.Println(string(out))
+		default:
+			for key, value := range values {
+				fmt.Printf("%s:%s\n", key, value)
+			}
+		}
+
 	} else {
 		value, err := ps.CollectSingle()
 		if err != nil {
 			log.Fatalf("Failed to read from parameter store. Error: %s", err)
 		}
+		if *flagBase64 {
+			value = b64(value)
+		}
 		fmt.Println(value)
 	}
+}
+
+func b64(data string) string {
+	return base64.StdEncoding.EncodeToString([]byte(data))
 }
